27b31ea78ff45c081fffb91adf9a7d1d0129efab
pinkymanga <orochi_alex@hotmail.com>
2014-11-12 17:19:14 -0600

    primer commit

---
+++ src/main/groovy/com/celulas/Celulas.groovy
@@ -0,0 +1,14 @@
+package com.celulas
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pinky
+ * Date: 11/12/14
+ * Time: 5:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Celulas {
+    def imprime() {
+      println  'Hola mundo'
+    }
+}
---
+++ src/test/groovy/com/celulas/CelulasSpec.groovy
@@ -0,0 +1,25 @@
+package com.celulas
+
+import spock.lang.Specification
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pinky
+ * Date: 11/12/14
+ * Time: 5:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class CelulasSpec extends Specification{
+
+    void "must print hola mundo"() {
+        setup:
+        Celulas celulas = new Celulas()
+
+        when:
+         def response = celulas.imprime()
+        then:
+         assert response == "Hola mundo"
+
+
+    }
+}
